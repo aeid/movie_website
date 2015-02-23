9f2688f4480db9bec7a84083e7fc0da966ca9f7a
Ahmed Eid <aeid101@gmail.com>
2015-02-22 18:19:31 -0800

    Delete movie.py

--- movie.py
+++
@@ -1,7 +0,0 @@
-class Movie():
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster = poster_image
-		self.trailer = trailer_youtube
-		
